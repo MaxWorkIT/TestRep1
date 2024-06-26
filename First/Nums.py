8ae177b95d3bd628a3e1b40b34d5e6fa6d58575d
Макс <rumyantsev1m0s1@gmail.com>
2024-06-26 15:39:34 +0300

    Добавлена функция div

--- First/Nums.py
+++ First/Nums.py
@@ -10,10 +10,15 @@ def mul(a, b):
     return a * b
 
 
+def div(a, b):
+    return a / b
+
+
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
     print(mul(3, 3))
+    print(div(6, 2))
 
 
 
