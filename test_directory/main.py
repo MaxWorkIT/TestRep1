32bbdc93ef44614870a0d39bb37581ddfb74aa9c
MaxWorkIT <rumyantsev1m0s1@gmail.com>
2024-07-10 20:48:31 +0300

    TIK-001: добавлен эндпоинт farewell
    Signed-off-by: Макс <rumyantsev1m0s1@gmail.com>

--- test_directory/main.py
+++ test_directory/main.py
@@ -7,4 +7,9 @@ app = FastAPI(
 
 @app.get(path="/greeting{name}", description="greeting by name")
 def greeting(name: str) -> str:
-    return f"Halo, {name}"
+    return f"Halo, {name}!"
+
+
+@app.get(path="/farewell{name}", description="farewell by name")
+def farewell(name: str) -> str:
+    return f"Bye, {name}!"
